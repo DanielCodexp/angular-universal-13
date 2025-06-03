54d4827d913f2e09576416f467c27aa58f2f63a5
Daniel Hernandez <116531997+DanielCodexp@users.noreply.github.com>
2025-06-02 18:12:12 -0600

    configuración de firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCBIeYSt24Z99CmwVmLKvP4t4aml4xfR6Y",
+    authDomain: "angular-universal-13.firebaseapp.com",
+    projectId: "angular-universal-13",
+    storageBucket: "angular-universal-13.firebasestorage.app",
+    messagingSenderId: "620571136794",
+    appId: "1:620571136794:web:8e1613ddbe40c876d752a3",
+    measurementId: "G-08B7WMLE53"
+  },
+  URL_API: 'http://localhost:8080/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,15 @@
 
 export const environment = {
   production: false,
+    firebase: {
+    apiKey: "AIzaSyCBIeYSt24Z99CmwVmLKvP4t4aml4xfR6Y",
+    authDomain: "angular-universal-13.firebaseapp.com",
+    projectId: "angular-universal-13",
+    storageBucket: "angular-universal-13.firebasestorage.app",
+    messagingSenderId: "620571136794",
+    appId: "1:620571136794:web:8e1613ddbe40c876d752a3",
+    measurementId: "G-08B7WMLE53"
+  },
   URL_API: 'http://localhost:8080/api',
 };
 
